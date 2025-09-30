2fe8ab12ef80fb71f136ee1928b616f8586a5f0f
CyberGhost-15 <apossible2019@gmail.com>
2025-09-30 09:04:12 +0100

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  apiKey: {
+    type: String,
+    unique: true,
+  },
+}, {
+  timestamps: true,
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  
+  if (!this.apiKey) {
+    this.apiKey = await bcrypt.hash(this._id.toString(), 8);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
